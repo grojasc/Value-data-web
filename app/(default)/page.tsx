56691c9c92ce55d57d2eade310b9f1af82eaf85e
Gonzalo Rojas <77162084+grojasc@users.noreply.github.com>
2025-03-25 18:52:50 -0300

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,25 +1,31 @@
+export const metadata = {
+  title: "Home - Open PRO",
+  description: "Page description",
+};
+
+import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
 import Workflows from "@/components/workflows";
 import Features from "@/components/features";
-import Testimonials from "@/components/testimonials";
-import Cta from "@/components/cta";
-
+@@ -13,12 +7,23 @@
 export default function Home() {
   return (
     <>
+      <PageIllustration />
       <Hero />
-      <section>
-        <h2>Nuestra experiencia</h2>
-        <p>Nos especializamos en las siguientes áreas:</p>
-        <ul>
-          <li>Ingeniería de datos</li>
-          <li>Arquitectura de datos</li>
-          <li>Gobernanza de datos y privacidad</li>
-          <li>IA generativa</li>
-          <li>Aprendizaje automático</li>
-          <li>Datos centrados en las personas</li>
-        </ul>
-      </section>
+		<section>
+			<h2>Nuestros Servicios</h2>
+			<p>Nos especializamos en:</p>
+			<ul>
+			  <li>Data Engineering</li>
+			  <li>Data Architecture</li>
+			  <li>Data Governance & Privacy</li>
+			  <li>Generative AI</li>
+			  <li>Machine Learning</li>
+			  <li>Human-Centered Data</li>
+			  <li>Optimization Models</li>
+			</ul>
+		</section>
       <Workflows />
       <Features />
       <Testimonials />
@@ -27,4 +33,3 @@ export default function Home() {
     </>
   );
 }
-
