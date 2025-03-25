18e4754a58ef2cc1b914fdb7723f9da44958fddf
Gonzalo Rojas <77162084+grojasc@users.noreply.github.com>
2025-03-25 17:56:58 -0300

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,9 +1,3 @@
-export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
-};
-
-import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
 import Workflows from "@/components/workflows";
 import Features from "@/components/features";
@@ -13,8 +7,19 @@ import Cta from "@/components/cta";
 export default function Home() {
   return (
     <>
-      <PageIllustration />
       <Hero />
+      <section>
+        <h2>Our Expertise</h2>
+        <p>We specialize in the following areas:</p>
+        <ul>
+          <li>Data Engineering</li>
+          <li>Data Architecture</li>
+          <li>Data Governance & Privacy</li>
+          <li>Generative AI</li>
+          <li>Machine Learning</li>
+          <li>Human-Centered Data</li>
+        </ul>
+      </section>
       <Workflows />
       <Features />
       <Testimonials />
