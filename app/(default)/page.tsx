40e2ff0d7c06f8f7c215fd0645a29c8ffcfba7eb
Gonzalo Rojas <77162084+grojasc@users.noreply.github.com>
2025-06-28 16:39:32 -0400

    Fix homepage syntax

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,31 +1,33 @@
+import PageIllustration from "@/components/page-illustration";
+import Hero from "@/components/hero-home";
+import Workflows from "@/components/workflows";
+import Features from "@/components/features";
+import Testimonials from "@/components/testimonials";
+import Cta from "@/components/cta";
+
 export const metadata = {
   title: "Home - Open PRO",
   description: "Page description",
 };
 
-import PageIllustration from "@/components/page-illustration";
-import Hero from "@/components/hero-home";
-import Workflows from "@/components/workflows";
-import Features from "@/components/features";
-@@ -13,12 +7,23 @@
 export default function Home() {
   return (
     <>
       <PageIllustration />
       <Hero />
-		<section>
-			<h2>Nuestros Servicios</h2>
-			<p>Nos especializamos en:</p>
-			<ul>
-			  <li>Data Engineering</li>
-			  <li>Data Architecture</li>
-			  <li>Data Governance & Privacy</li>
-			  <li>Generative AI</li>
-			  <li>Machine Learning</li>
-			  <li>Human-Centered Data</li>
-			  <li>Optimization Models</li>
-			</ul>
-		</section>
+      <section>
+        <h2>Nuestros Servicios</h2>
+        <p>Nos especializamos en:</p>
+        <ul>
+          <li>Data Engineering</li>
+          <li>Data Architecture</li>
+          <li>Data Governance & Privacy</li>
+          <li>Generative AI</li>
+          <li>Machine Learning</li>
+          <li>Human-Centered Data</li>
+          <li>Optimization Models</li>
+        </ul>
+      </section>
       <Workflows />
       <Features />
       <Testimonials />
