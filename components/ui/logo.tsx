6fa0aefbf6a64f858f211cc233582be2a44ba934
Gonzalo Rojas <77162084+grojasc@users.noreply.github.com>
2025-07-05 12:04:01 -0400

    Remove local logo image and use external URL

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/public/images/logo.svg";
+const logoUrl =
+  "https://raw.githubusercontent.com/grojasc/Value-data-web/master/Value%20Data%20Logo.png";
 
 export default function Logo() {
   return (
-    <Link href="/" className="inline-flex shrink-0" aria-label="Cruip">
-      <Image src={logo} alt="Cruip Logo" width={32} height={32} />
+    <Link href="/" className="inline-flex shrink-0" aria-label="Value Data">
+      <Image src={logoUrl} alt="Value Data Logo" width={32} height={32} />
     </Link>
   );
 }
